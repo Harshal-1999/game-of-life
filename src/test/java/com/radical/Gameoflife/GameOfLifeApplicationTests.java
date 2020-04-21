804cb34e2dd4c486fba6c60b5d0b706c19ad4739
Harshal-1999 <Harshalb@27>
2020-04-21 13:22:13 +0530

    upload

---
+++ src/test/java/com/radical/Gameoflife/GameOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radical.Gameoflife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
